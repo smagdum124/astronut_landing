eb1f83853abda134a6e3e7f15e065985fb331a46
Magdum shaikh <94826076+smagdum124@users.noreply.github.com>
2024-05-25 18:07:26 +0530

    Initial commit

--- src/Components/Achievment.jsx
+++ src/Components/Achievment.jsx
@@ -1,7 +1,8 @@
-import { FiArrowDownRight } from "react-icons/fi";
 import pose2 from "../assets/pose9 1.png";
 import "./style.css";
-
+import rectangle from "../assets/Rectangle 4058.png";
+import arrow from "../assets/Group 9268.png";
+import know from "../assets/Group 9222.png";
 const Achievement = () => {
   return (
     <div className="achievement-container">
@@ -11,12 +12,27 @@ const Achievement = () => {
       <div className="achievement-content">
         <div className="achievement-img">
           <img src={pose2} alt="" />
+          <div className="know-more">
+            <img style={{ width: "auto" }} src={know} alt="" />{" "}
+            <span>Know more </span>
+          </div>
         </div>
         <div className="achievement-text">
-          <h2 >Already above</h2>
-          <h2 style={{color:"aqua"}} className="highlight"> 
-            <span>the</span> Surface
+          <h2 className="achiev-txt2">Already above</h2>
+          <h2>
+            <span>the</span> <span className="highlight">Surface</span>
           </h2>
+          <div className="image-container">
+            <div className="image-with-text">
+              <img src={rectangle} alt="" />
+              <p>First Image Text</p>
+            </div>
+            <div className="image-with-text">
+              <img src={rectangle} alt="" />
+              <p>Second Image Text</p>
+            </div>
+            <img style={{ width: "11vw", height: "10vw" }} src={arrow} alt="" />
+          </div>
         </div>
       </div>
     </div>
